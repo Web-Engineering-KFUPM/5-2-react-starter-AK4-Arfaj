0091bca9853878453a16b9ffcb5a330804dc6b06
AK4-Arfaj <ak4.arfaj@gmail.com>
2025-10-01 11:25:41 +0300

    Completed TODO1 - Created a react component called StudentCard which displays name, id and department of a student. Also added a StudentCard component in app.jsx

---
+++ 5-2-react-starter/src/components/StudentCard.jsx
@@ -0,0 +1,11 @@
+function StudentCard() {
+    return (
+        <div>
+            <h3>Shayma</h3>
+            <p>202256400</p>
+            <p>ICS</p>
+        </div>
+    );
+}
+
+export default StudentCard;
