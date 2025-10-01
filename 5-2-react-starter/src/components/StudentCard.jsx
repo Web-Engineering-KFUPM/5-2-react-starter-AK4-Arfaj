ef2204802deb56df6f141c7531d1620437cd77c5
AK4-Arfaj <ak4.arfaj@gmail.com>
2025-10-01 11:51:15 +0300

    Added styling for the student card component

--- 5-2-react-starter/src/components/StudentCard.jsx
+++ 5-2-react-starter/src/components/StudentCard.jsx
@@ -1,6 +1,6 @@
 function StudentCard(props) {
     return (
-        <div>
+        <div className="student-card">
             <h3>Name: {props.name}</h3>
             <p>Student ID: {props.id}</p>
             <p>Department: {props.dept}</p>
